33e46a75960a088433191dd11f52dd6039dbb8ff
rutul <rutul591@gmail.com>
2015-06-05 17:08:39 -0700

    added connected/disconnected user

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var expres = require('express');
+var express = require('express');
+var io = require("socket.io")(http);
+
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log(" A user is connected for chat");
+	socket.on('disconnect', function(){
+		console.log("A user has disconnected from chat");
+	}); 
 });
 
 http.listen(3000,function(){
