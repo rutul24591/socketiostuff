51dee437339fe6ffb92c6bdfacdf14e0e22868f4
rutul <rutul591@gmail.com>
2015-06-05 18:40:17 -0700

    added chat functionality

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ var http = require('http').Server(app);
 var express = require('express');
 var io = require("socket.io")(http);
 
-
+//var username;
+//var typing= false;
+//var connected= false;
+//var $usernameInput = $('.usernameInput'); // input for user's Username
+//var $currentInput = $usernameInput.focus();
+ 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log(" A user is connected for chat");
-	socket.on('disconnect', function(){
-		console.log("A user has disconnected from chat");
+	socket.emit('connected');
+	socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
 	}); 
 });
+
+	function updateTyping () {
+	    if (connected) {
+    	  if (!typing) {
+        typing = true;
+        socket.emit('typing');
+      }
+      lastTypingTime = (new Date()).getTime();
+	}
+} 
 
 http.listen(3000,function(){
 	console.log(" Http server listening on port 3000");
